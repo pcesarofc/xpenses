2c8d34649905211157da18c22a168279e252990c
pcesarofc <p.cesarofc@gmail.com>
2021-06-29 19:27:20 -0300

    aula chart widget 5

--- lib/components/chart_bar.dart
+++ lib/components/chart_bar.dart
@@ -10,24 +10,50 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(
-          'R\$${value.toStringAsFixed(2)}',
-        ),
-        SizedBox(
-          height: 5,
-        ),
-        SizedBox(
-          height: 5,
-        ),
-        Container(
-          height: 60,
-          width: 10,
-          child: null,
-        ),
-        Text(label),
-      ],
+    return Container(
+      width: 45,
+      child: Column(
+        children: [
+          Text(
+            '${value.toStringAsFixed(2)}',
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Container(
+            height: 60,
+            width: 10,
+            child: Stack(
+              alignment: Alignment.bottomCenter,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.grey,
+                      width: 1.0,
+                    ),
+                    color: Color.fromRGBO(220, 220, 220, 1),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+                FractionallySizedBox(
+                  heightFactor: percentage,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      color: Colors.blue,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(label),
+        ],
+      ),
     );
   }
 }
